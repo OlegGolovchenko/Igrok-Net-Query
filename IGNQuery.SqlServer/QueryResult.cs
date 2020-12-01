f1d751897a57a21c2c573de1e0795ca15811c99f
Oleg Golovchenko <IgRoK_be@hotmail.com>
2020-12-01 22:18:06 +0100

    added old code from igndata to ignquery

---
+++ IGNQuery.SqlServer/QueryResult.cs
@@ -0,0 +1,19 @@
+﻿using IGNQuery.Interfaces.QueryProvider;
+
+namespace IGNQuery.SqlServer
+{
+    internal class QueryResult : IQueryResult
+    {
+        private readonly string _query;
+
+        public QueryResult(string query)
+        {
+            _query = query;
+        }
+
+        public string GetResultingString()
+        {
+            return _query;
+        }
+    }
+}
