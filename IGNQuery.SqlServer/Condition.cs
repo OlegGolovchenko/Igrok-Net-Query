ddadc5daae7e4b58f78ec884dde63230d419fb7c
Oleg Golovchenko <IgRoK_be@hotmail.com>
2020-12-01 22:18:06 +0100

    added old code from igndata to ignquery

---
+++ IGNQuery.SqlServer/Condition.cs
@@ -0,0 +1,104 @@
+﻿using IGNQuery.Interfaces.QueryProvider;
+
+namespace IGNQuery.SqlServer
+{
+    internal class Condition : ICondition
+    {
+        private string _query;
+
+        public Condition(string query)
+        {
+            _query = query;
+        }
+
+        public ICondition And()
+        {
+            _query += "AND ";
+            return new Condition(_query);
+        }
+
+        public ICondition BoolEqual(bool value)
+        {
+            _query += $"= {(value ? 1 : 0)} ";
+            return new Condition(_query);
+        }
+
+        public ICondition BoolEqualToParam(int paramNb)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public ICondition BoolNotEqual(bool value)
+        {
+            _query += $"!= {(value ? 1 : 0)} ";
+            return new Condition(_query);
+        }
+
+        public ICondition BoolNotEqualToParam(int paramNb)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public ICondition Field(string name)
+        {
+            _query += $"{name} ";
+            return new Condition(_query);
+        }
+
+        public IQueryResult Go()
+        {
+            _query += ";";
+            return new QueryResult(_query);
+        }
+
+        public ICondition LongEqual(long value)
+        {
+            _query += $"= {value} ";
+            return new Condition(_query);
+        }
+
+        public ICondition LongEqualToParam(int paramNb)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public ICondition LongNotEqual(long value)
+        {
+            _query += $"!= {value} ";
+            return new Condition(_query);
+        }
+
+        public ICondition LongNotEqualToParam(int paramNb)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public ICondition Or()
+        {
+            _query += "OR ";
+            return new Condition(_query);
+        }
+
+        public ICondition StringEqual(string value)
+        {
+            _query += $"= '{value}' ";
+            return new Condition(_query);
+        }
+
+        public ICondition StringEqualToParam(int paramNb)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public ICondition StringNotEqual(string value)
+        {
+            _query += $"!= '{value}' ";
+            return new Condition(_query);
+        }
+
+        public ICondition StringNotEqualToParam(int paramNb)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ IGNQuery.SqlServer/MSSQLCreateQuery.cs
@@ -0,0 +1,88 @@
+﻿using IGNQuery.Interfaces.QueryProvider;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IGNQuery.SqlServer
+{
+    internal class MSSQLCreateQuery : ICreateQuery
+    {
+        private string _query;
+
+        public MSSQLCreateQuery(string query)
+        {
+            _query = query;
+        }
+
+        public string GetResultingString()
+        {
+            return _query;
+        }
+
+        public IQueryResult StoredProcedureIfNotExists(string name, IQueryResult content, IEnumerable<TableField> parameters = null)
+        {
+            var query = new StringBuilder();
+            query.Append("IF OBJECT_ID('");
+            query.Append(name);
+            query.Append("', 'P') IS NOT NULL");
+            query.AppendLine();
+            query.Append("DROP PROCEDURE ");
+            query.Append(name);
+            query.Append("GO");
+            query.AppendLine();
+            query.Append("CREATE STORED PROCEDURE ");
+            query.Append(name);
+            query.Append(" ");
+            if (parameters != null)
+            {
+                foreach (var col in parameters)
+                {
+                    query.Append(col.Name);
+                    query.Append(" ");
+                    query.Append(col.Type);
+                    query.Append(", ");
+                }
+            }
+            query.AppendLine();
+            query.Append(" AS ");
+            query.Append(content);
+            query.Append("GO");
+            _query += query;
+            return new QueryResult(_query);
+        }
+
+        public IQueryResult TableIfNotExists(string name, IEnumerable<TableField> fields)
+        {
+            var query = new StringBuilder();
+            query.Append("IF NOT EXISTS(SELECT * FROM sysobjects WHERE name='");
+            query.Append(name);
+            query.Append("' and xtype='U')");
+            query.AppendLine();
+            query.Append("CREATE TABLE ");
+            query.Append(name);
+            query.Append("(");
+            foreach (var col in fields)
+            {
+                query.Append(col.Name);
+                query.Append(" ");
+                query.Append(col.Type);
+                query.Append(" ");
+                query.Append(col.CanHaveNull ? "null" : "not null");
+                if (!string.IsNullOrEmpty(col.DefValue))
+                {
+                    query.Append(" default ");
+                    query.Append(col.DefValue);
+                }
+                if (col.Generated)
+                {
+                    query.Append(" IDENTITY(1,1)");
+                }
+                query.Append(", ");
+            }
+            query.Append(")GO");
+
+            _query += query;
+
+            return new QueryResult(_query);
+        }
+    }
+}
