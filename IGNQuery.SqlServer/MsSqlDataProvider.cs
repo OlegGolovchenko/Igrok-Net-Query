2763c7fbf2734a056a5e9a758c80b29fed09b7df
Oleg <igrok_be@hotmail.com>
2020-12-02 20:47:40 +0100

    changes for version 1.0.2 of ignquery and 1.0.0 of ignquery.sqlserver (#5)
    * added sql dialect enum to centralized dll
    
    * added old code from igndata to ignquery
    
    * added more query definitions for sqls server
    added parametrized parts to update query

---
+++ IGNQuery.SqlServer/MsSqlDataProvider.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Data.Common;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Logging;
+using IGNQuery.Interfaces;
+using IGNQuery.Interfaces.QueryProvider;
+using System.Collections.Generic;
+using IGNQuery.Enums;
+
+namespace IGNQuery.SqlServer
+{
+    public class MsSqlDataProvider : IDataProvider
+    {
+        private readonly string _connectionString;
+        private SqlConnection _connection;
+        private ILogger _logger;
+
+        public bool queryToOutput = false;
+
+        public MsSqlDataProvider(ILogger logger)
+        {
+            _logger = logger;
+            _connectionString = Environment.GetEnvironmentVariable("SQLSERVER_CONNECTION_STRING");
+        }
+
+        public void ExecuteNonQuery(IQueryResult query)
+        {
+            ResetConnection();
+            if (_connection == null)
+            {
+                _connection = new SqlConnection(_connectionString);
+                _connection.Open();
+            }
+            if (queryToOutput)
+            {
+                Console.WriteLine(query.GetResultingString());
+            }
+            var command = new SqlCommand(query.GetResultingString(), _connection);
+            command.ExecuteNonQuery();
+        }
+
+        public void ExecuteNonQueryWithParams(IQueryResult query, IEnumerable<ParameterValue> parameters)
+        {
+            ResetConnection();
+            if (_connection == null)
+            {
+                _connection = new SqlConnection(_connectionString);
+                _connection.Open();
+            }
+            if (queryToOutput)
+            {
+                Console.WriteLine(query.GetResultingString());
+            }
+            var command = new SqlCommand(query.GetResultingString(), _connection);
+            foreach (var param in parameters)
+            {
+                command.Parameters.AddWithValue($"@p{param.ParamNumber}", param.ParamValue);
+            }
+            command.ExecuteNonQuery();
+        }
+
+        public void ExecuteStoredProcedure(string procname, IEnumerable<ParameterValue> parameters = null)
+        {
+            ResetConnection();
+            if (_connection == null)
+            {
+                _connection = new SqlConnection(_connectionString);
+                _connection.Open();
+            }
+            if (queryToOutput)
+            {
+                Console.WriteLine($"calling stored procedure [{procname}]");
+            }
+            var command = new SqlCommand(procname, _connection);
+            command.CommandType = System.Data.CommandType.StoredProcedure;
+            if (parameters != null)
+            {
+                foreach (var param in parameters)
+                {
+                    command.Parameters.AddWithValue($"@p{param.ParamNumber}", param.ParamValue);
+                }
+            }
+            command.ExecuteNonQuery();
+        }
+
+        public DbDataReader ExecuteReader(IQueryResult query)
+        {
+            ResetConnection();
+            if (_connection == null)
+            {
+                _connection = new SqlConnection(_connectionString);
+                _connection.Open();
+            }
+            if (queryToOutput)
+            {
+                Console.WriteLine(query.GetResultingString());
+            }
+            var command = new SqlCommand(query.GetResultingString(), _connection);
+            DbDataReader result = command.ExecuteReader();
+            return result;
+        }
+
+        public DbDataReader ExecuteReaderWithParams(IQueryResult query, IEnumerable<ParameterValue> parameters)
+        {
+            ResetConnection();
+            if (_connection == null)
+            {
+                _connection = new SqlConnection(_connectionString);
+                _connection.Open();
+            }
+            if (queryToOutput)
+            {
+                Console.WriteLine(query.GetResultingString());
+            }
+            var command = new SqlCommand(query.GetResultingString(), _connection);
+            foreach (var param in parameters)
+            {
+                command.Parameters.AddWithValue($"@p{param.ParamNumber}", param.ParamValue);
+            }
+            DbDataReader result = command.ExecuteReader();
+            return result;
+        }
+
+        public DbDataReader ExecuteStoredProcedureReader(string procname, IEnumerable<ParameterValue> parameters = null)
+        {
+            ResetConnection();
+            if (_connection == null)
+            {
+                _connection = new SqlConnection(_connectionString);
+                _connection.Open();
+            }
+            if (queryToOutput)
+            {
+                Console.WriteLine($"calling stored procedure [{procname}]");
+            }
+            var command = new SqlCommand(procname, _connection);
+            command.CommandType = System.Data.CommandType.StoredProcedure;
+            if (parameters != null)
+            {
+                foreach (var param in parameters)
+                {
+                    command.Parameters.AddWithValue($"@p{param.ParamNumber}", param.ParamValue);
+                }
+            }
+            DbDataReader result = command.ExecuteReader();
+            return result;
+        }
+
+        public IQuery Query()
+        {
+            return new SqlQuery() { Dialect = DialectEnum.MSSQL };
+        }
+
+        public void ResetConnection()
+        {
+            try
+            {
+                _connection.Close();
+                _connection.Dispose();
+            }
+            catch (Exception e)
+            {
+
+            }
+            _connection = null;
+        }
+
+        public void Dispose()
+        {
+            try
+            {
+                _connection.Close();
+            }
+            catch (Exception e)
+            {
+
+            }
+            finally
+            {
+
+                _connection.Dispose();
+                _connection = null;
+            }
+        }
+    }
+}
---
+++ IGNQuery.SqlServer/UpdateQuery.cs
@@ -0,0 +1,73 @@
+﻿using IGNQuery.Interfaces.QueryProvider;
+
+namespace IGNQuery.SqlServer
+{
+    internal class UpdateQuery : IUpdateQuery
+    {
+        private string _query;
+
+        public UpdateQuery(string query)
+        {
+            _query = query;
+        }
+
+        public string GetResultingString()
+        {
+            return _query;
+        }
+
+        public IQueryResult SetBooleanFieldFalse(string field)
+        {
+            _query += $"SET {field} = 0;";
+            return new UpdateQuery(_query);
+        }
+
+        public IConditionalQuery SetBooleanFieldFalseWithCondition(string field)
+        {
+            _query += $"SET {field} = 0 ";
+            return new ConditionalQuery(_query);
+        }
+
+        public IQueryResult SetBooleanFieldTrue(string field)
+        {
+            _query += $"SET {field} = 1;";
+            return new UpdateQuery(_query);
+        }
+
+        public IConditionalQuery SetBooleanFieldTrueWithCondition(string field)
+        {
+            _query += $"SET {field} = 1 ";
+            return new ConditionalQuery(_query);
+        }
+
+        public IQueryResult SetLongField(string field, long value)
+        {
+            _query += $"SET {field} = {value};";
+            return new UpdateQuery(_query);
+        }
+
+        public IConditionalQuery SetLongFieldWithCondition(string field, long value)
+        {
+            _query += $"SET {field} = {value} ";
+            return new ConditionalQuery(_query);
+        }
+
+        public IQueryResult SetStringField(string field, string value)
+        {
+            _query += $"SET {field} = '{value}';";
+            return new UpdateQuery(_query);
+        }
+
+        public IConditionalQuery SetStringFieldWithCondition(string field, string value)
+        {
+            _query += $"SET {field} = '{value}' ";
+            return new ConditionalQuery(_query);
+        }
+
+        public IUpdateQuery Table(string table)
+        {
+            _query += $"UPDATE {table} ";
+            return new UpdateQuery(_query);
+        }
+    }
+}
